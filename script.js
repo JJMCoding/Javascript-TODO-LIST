593a463ebf40e49ea2ebd9f819d51d16fe5fed13
James McKay <jamesmckay12345@icloud.com>
2022-05-24 09:01:19 +0100

    update

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-//get submitBTN and change it into a JS var
+
 const submitBTN = document.getElementById("submit");
 const clearBTN = document.getElementById("clear-list");
 const textAppend = document.getElementById("append-text");
 
-//create variable for the text input to add to append-text
-//create function which onclick of submit button
-//will allow us to append our value of textInput
-//to the div append-text in a li format
+
 submitBTN.addEventListener('click', function() {
   const textInput = document.getElementById("text-input").value;
+  const textClear = document.getElementById("text-input").value = "";
+  let div = document.createElement('div');
+  div.setAttribute('class', 'div');  
   let list = document.createElement('li');
   let item = document.createTextNode(textInput);
   const delButton = document.createElement('button');
+  delButton.textContent = 'Delete';
+  delButton.setAttribute('class', 'btn');  
+  div.appendChild(list);
   list.appendChild(item);
-  list.appendChild(delButton);
-  document.getElementById('append-text').appendChild(list);
+  div.appendChild(delButton);
+  document.getElementById('append-text').appendChild(div);
   delButton.addEventListener('click', function(){
-    const listDel = document.querySelectorAll('li');
-    list.innerHTML = '';
+    textAppend.removeChild(div);
   })
 })
 
